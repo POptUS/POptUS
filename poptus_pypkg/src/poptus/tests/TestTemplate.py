596808b13c2068ac95693c126d6ce761e09cc7bb
Jared O'Neal <joneal@anl.gov>
2025-10-10 12:25:56 -0500

    (Issue #1) First draft of minimal package structure.
    All tox tasks passing.

---
+++ poptus_pypkg/src/poptus/tests/TestTemplate.py
@@ -0,0 +1,16 @@
+"""
+Automatic unittest of the XXX funtionality
+"""
+
+import unittest
+
+
+class TestTemplate(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testNothing(self):
+        self.assertTrue(True)
