5f28d675d7c1782d7210e1081499e10a49d2d063
Jared O'Neal <joneal@anl.gov>
2025-10-27 08:27:12 -0500

    (Issue #15) Revert final breakage.  This should be back to final form for merge.
    Both the sdist and wheel test subjobs failed as expected.

--- poptus_pypkg/src/poptus/tests/TestAbstractLogger.py
+++ poptus_pypkg/src/poptus/tests/TestAbstractLogger.py
@@ -12,4 +12,3 @@ class TestAbstractLogger(unittest.TestCase):
         # Confirm that class is abstract and cannot be instantiated
         with self.assertRaises(TypeError):
             poptus.AbstractLogger(poptus.LOG_LEVEL_DEFAULT)
-        self.assertTrue(False)
